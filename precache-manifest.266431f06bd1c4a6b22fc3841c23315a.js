7c39cfc075dd533628aa2bbc930006c8bda4b47f
acouch <acinternets@gmail.com>
2019-03-06 15:23:18 -0500

    Updates

---
+++ precache-manifest.266431f06bd1c4a6b22fc3841c23315a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "d3a0652ba03c7513889a920a7a650c49",
+    "url": "/catalog-admin/static/media/logo.d3a0652b.svg"
+  },
+  {
+    "revision": "de9cb7d726fed0e3bb3d05bc90a67885",
+    "url": "/catalog-admin/static/media/analytics-mock.de9cb7d7.png"
+  },
+  {
+    "revision": "9f11d561c7fc5a17d5bb",
+    "url": "/catalog-admin/static/js/runtime~main.9f11d561.js"
+  },
+  {
+    "revision": "c8524d3f7979fe223a45",
+    "url": "/catalog-admin/static/js/main.c8524d3f.chunk.js"
+  },
+  {
+    "revision": "6d18b5f60921abd9297b",
+    "url": "/catalog-admin/static/js/2.6d18b5f6.chunk.js"
+  },
+  {
+    "revision": "c8524d3f7979fe223a45",
+    "url": "/catalog-admin/static/css/main.a7f10b21.chunk.css"
+  },
+  {
+    "revision": "6d18b5f60921abd9297b",
+    "url": "/catalog-admin/static/css/2.13be4dfc.chunk.css"
+  },
+  {
+    "revision": "e1abd05a5e86fb64b90e1cf589e540a3",
+    "url": "/catalog-admin/index.html"
+  }
+];
